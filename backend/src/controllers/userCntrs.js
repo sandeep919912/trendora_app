85d6182a6ef0c365cb4af4feebb29a3d30645ded
Sandeep pandit <sandeeppandit919912@gmail.com>
2026-01-10 12:55:01 +0530

    cookie updation

--- backend/src/controllers/userCntrs.js
+++ backend/src/controllers/userCntrs.js
@@ -75,8 +75,8 @@ const loginUser = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
