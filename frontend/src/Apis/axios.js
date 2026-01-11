9bfd14fc754290cf20a2962571963ab19aeef927
Sandeep pandit <sandeeppandit919912@gmail.com>
2026-01-11 23:15:32 +0530

    image updation

--- frontend/src/Apis/axios.js
+++ frontend/src/Apis/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: "https://trendora-app.onrender.com",
   withCredentials: true,
 });
 
